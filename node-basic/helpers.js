ddd1ee8580e7d7868098ab81bec90c4ee2c5130c
Pedro Garboza <p.garboza@gmail.com>
2019-04-28 23:13:27 -0400

    Adding Hello World, Adding .gitignore

---
+++ node-basic/helpers.js
@@ -0,0 +1,22 @@
+/*function sum(a, b){
+	return a + b;
+}*/
+
+/*const sum = (a, b) => {
+
+	return a + b;
+
+};*/
+
+/*const sum = (a, b) => a + b;
+
+module.exports = {
+
+	sum
+
+};*/
+
+exports.sum = (a, b) => a + b;
+exports.add = (a, b) => a + b;
+
+/*console.log("PROCESS: ", process);*/
