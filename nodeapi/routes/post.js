ce3b987949b7a8e2ac7ed0e67d430a6749a03ed5
Pedro Garboza <p.garboza@gmail.com>
2019-05-05 23:26:40 -0400

    Implemented POST in place of PUT, route created. Client side implementation still in the works

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -32,24 +32,20 @@ router.get("/people", function(req, res){
 	res.sendFile(path.join(__dirname, "../people.json"));
 });
 
+//POST
 router.post("/addPerson", function(req, res){
 
   var jsonFile = fs.readFileSync("people.json")
   var jsonContent = JSON.parse(jsonFile);
 
-/*
+  /*
   console.log("Message!!!");
-
   res.end(JSON.stringify(req.body));
-
   console.log(req.body.firstNameName);
-
   console.log(req.body.lastNameName);
-
   console.log(req.body.emailName);
- 
   console.log(JSON.stringify(req.body));
-*/
+  */
 
   var newPerson = {
   				"firstName":req.body.firstNameName, 
@@ -66,7 +62,7 @@ router.post("/addPerson", function(req, res){
 	
   }
 
-/*{
+  /*{
 	"person":
 	[
 		{"firstName": "John", "lastName": "Doe", "email": "john@example.com"},
@@ -74,7 +70,7 @@ router.post("/addPerson", function(req, res){
 		{"firstName": "July", "lastName": "Dooley", "email": "july@example.com"},
 		{"firstName": "July", "lastName": "Dooley", "email": "july@example.com"}
 	]
-}*/
+  }*/
 
 	fs.writeFile("people.json", '{"person":'+JSON.stringify(jsonContent.person)+'}', (err) => {
 	 	if (err) console.log(err);
@@ -83,6 +79,61 @@ router.post("/addPerson", function(req, res){
 	});
 
 	res.sendFile(path.join(__dirname, "../create.html"));
+
+});
+
+//"PUT"
+router.post("/updatePerson", function(req, res){
+
+  var jsonFile = fs.readFileSync("people.json")
+  var jsonContent = JSON.parse(jsonFile);
+  /*var updatePerson = {
+  	"id":req.body.newPersonId,
+  	"firstName":req.body.firstNameName, 
+	"lastName":req.body.lastNameName, 
+	"email":req.body.emailName
+  };*/
+  var updatePerson = {
+  	"id":req.body.personToUpdateId,
+  	"firstName":req.body.firstNameName, 
+	"lastName":req.body.lastNameName, 
+	"email":req.body.emailName
+  };
+ 
+
+  for(i = 0; i < jsonContent.person.length; i++){
+  	if(req.body.personToUpdateId === jsonContent.person[i].id){
+  		console.log(req.body.personToUpdateId);
+  		console.log(updatePerson.firstName);
+  		console.log(updatePerson.lastName);
+  		console.log(updatePerson.email);
+
+  		/*console.log("id: ", jsonContent.person[i].id);
+  		console.log("firstname: ", jsonContent.person[i].firstName);
+		console.log("secondname: ", jsonContent.person[i].lastName);
+		console.log("email: ", jsonContent.person[i].email);*/
+
+  		//jsonContent.person.slice(1, 1);
+  		//jsonContent.person[i].push(updatePerson);
+		jsonContent.person[i].firstName = updatePerson.firstName;
+	  	jsonContent.person[i].lastName = updatePerson.lastName;
+	  	jsonContent.person[i].email = updatePerson.email;
+	}
+  } 
+
+  for(i = 0; i < jsonContent.person.length; i++){
+  	console.log("id: ", jsonContent.person[i].id);
+  	console.log("firstname: ", jsonContent.person[i].firstName);
+	console.log("secondname: ", jsonContent.person[i].lastName);
+	console.log("email: ", jsonContent.person[i].email);
+  }
+
+  fs.writeFile("people.json", '{"person":'+JSON.stringify(jsonContent.person)+'}', (err) => {
+	if (err) console.log(err);
+	console.log("Successfully Written to File.");
+  });
+
+  res.sendFile(path.join(__dirname, "../update.html"));
 
 });
 
